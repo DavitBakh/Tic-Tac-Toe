4914545050b9dce7e2a61cd8b45bbca47d053f2d
DavitBakh <davobakhshyan@gmail.com>
2022-08-24 23:02:04 +0400

    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Tic_Tac_Toe.UI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Controls.Add(this.btn9, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btn8, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btn7, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btn6, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btn5, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btn4, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btn3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btn2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btn1, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(251, 77);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(300, 300);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // btn9
+            // 
+            this.btn9.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn9.Location = new System.Drawing.Point(201, 201);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(94, 94);
+            this.btn9.TabIndex = 8;
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn8.Location = new System.Drawing.Point(102, 201);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(93, 94);
+            this.btn8.TabIndex = 7;
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn7.Location = new System.Drawing.Point(3, 201);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(93, 94);
+            this.btn7.TabIndex = 6;
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.BackColor = System.Drawing.SystemColors.Control;
+            this.btn6.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btn6.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn6.Location = new System.Drawing.Point(201, 102);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(94, 93);
+            this.btn6.TabIndex = 5;
+            this.btn6.UseVisualStyleBackColor = false;
+            this.btn6.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn5.Location = new System.Drawing.Point(102, 102);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(93, 93);
+            this.btn5.TabIndex = 4;
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn4.Location = new System.Drawing.Point(3, 102);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(93, 93);
+            this.btn4.TabIndex = 3;
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn3.Location = new System.Drawing.Point(201, 3);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(94, 93);
+            this.btn3.TabIndex = 2;
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn2.Location = new System.Drawing.Point(102, 3);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(93, 93);
+            this.btn2.TabIndex = 1;
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn1.Location = new System.Drawing.Point(3, 3);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(93, 93);
+            this.btn1.TabIndex = 0;
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private Button btn9;
+        private Button btn8;
+        private Button btn7;
+        private Button btn6;
+        private Button btn5;
+        private Button btn4;
+        private Button btn3;
+        private Button btn2;
+        private Button btn1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,232 @@
+namespace Tic_Tac_Toe.UI
+{
+    public partial class Form1 : Form
+    {
+        char userChar = 'X';
+        char computerChar = 'O';
+        char emptyChar = '_';
+
+        bool User = false;
+        bool AI = true;
+
+
+
+        char[,] board;
+        bool canMove = true;
+
+        Button[,] buttons;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            board = new char[3, 3]
+            {
+                { emptyChar, emptyChar, emptyChar },
+                { emptyChar, emptyChar, emptyChar },
+                {emptyChar,emptyChar, emptyChar }
+            };
+
+            buttons = new Button[3, 3]
+             {
+                { btn1, btn2, btn3 },
+                { btn4, btn5, btn6 },
+                { btn7, btn8, btn9 }
+             };
+
+        }
+
+
+        public void UpdateField()
+        {
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    if (board[i, j] == emptyChar)
+                        continue;
+                    if (board[i, j] == 'X')
+                        buttons[i, j].Text = "X";
+                    else
+                        buttons[i, j].Text = "O";
+                }
+            }
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            Button? button = sender as Button;
+
+            if (button == null)
+                return;
+
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    if (canMove && button == buttons[i, j])
+                    {
+                        if (board[i, j] != emptyChar)
+                            return;
+
+                        board[i, j] = userChar;
+                        UpdateField();
+                        canMove = false;
+
+                        if (isWin(userChar, board))
+                        {
+                            MessageBox.Show("Win Player " + userChar);
+                            Close();
+                        }
+
+                        ComputerMove(board);
+                        UpdateField();
+                        if (isWin(computerChar, board))
+                        {
+                            MessageBox.Show("Win Player " + computerChar);
+                            Close();
+                        }
+                        if (isDraw(board))
+                        {
+                            MessageBox.Show("Íè÷ÿ");
+                            Close();
+                        }
+
+                        return;
+                    }
+                }
+            }
+        }
+
+        public void ComputerMove(char[,] board)
+        {
+            int bestScore = int.MinValue;
+            char[,] copBoard = (char[,])board.Clone();
+
+            int y = -1;
+            int x = -1;
+            for (int i = 0; i < copBoard.GetLength(0); i++)
+            {
+                for (int j = 0; j < copBoard.GetLength(1); j++)
+                {
+                    if (copBoard[i, j] == emptyChar)
+                    {
+                        copBoard[i, j] = computerChar;
+                        int score = MinMax(copBoard, User);
+                        copBoard[i, j] = emptyChar;
+                        if (score > bestScore)
+                        {
+                            bestScore = score;
+                            y = i;
+                            x = j;
+
+                        }
+
+                    }
+                }
+            }
+            if (y != -1 && x != -1)
+            {
+                board[y, x] = computerChar;
+                canMove = true;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            UpdateField();
+        }
+
+        public int MiniMax(char[,] board, bool minMax)
+        {
+            if (isWin(userChar, board))
+                return -10;
+            if (isWin(computerChar, board))
+                return 10;
+            if (isDraw(board))
+                return 0;
+
+
+            if (minMax)
+            {
+                int bestScore = int.MinValue;
+                for (int i = 0; i < board.GetLength(0); i++)
+                {
+                    for (int j = 0; j < board.GetLength(1); j++)
+                    {
+                        if (board[i, j] == emptyChar)
+                        {
+                            board[i, j] = computerChar;
+                            int score = MinMax(board, User);
+                            board[i, j] = emptyChar;
+                            bestScore = Math.Max(bestScore, score);
+                        }
+                    }
+                }
+                return bestScore;
+            }
+            else
+            {
+                int bestScore = int.MaxValue;
+                for (int i = 0; i < board.GetLength(0); i++)
+                {
+                    for (int j = 0; j < board.GetLength(1); j++)
+                    {
+                        if (board[i, j] == emptyChar)
+                        {
+                            board[i, j] = userChar;
+                            int score = MinMax(board, AI);
+
+                            board[i, j] = emptyChar;
+                            bestScore = Math.Min(bestScore, score);
+                        }
+                    }
+                }
+                return bestScore;
+            }
+
+        }
+
+
+        public bool isWin(char ch, char[,] board)
+        {
+            for (int y = 0; y < board.GetLength(0); y++)
+            {
+                bool CheckX = true;
+                bool CheckY = true;
+                for (int x = 0; x < board.GetLength(1); x++)
+                {
+                    if (board[y, x] != ch)
+                        CheckY = false;
+
+                    if (board[x, y] != ch)
+                        CheckX = false;
+                }
+                if (CheckX || CheckY)
+                    return true;
+            }
+
+            if (board[0, 0] == ch && board[1, 1] == ch && board[2, 2] == ch)
+                return true;
+
+            if (board[0, 2] == ch && board[1, 1] == ch && board[2, 0] == ch)
+                return true;
+
+            return false;
+        }
+
+        public bool isDraw(char[,] board)
+        {
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    if (board[i, j] == emptyChar)
+                        return false;
+                }
+            }
+            return true;
+        }
+    }
+}
